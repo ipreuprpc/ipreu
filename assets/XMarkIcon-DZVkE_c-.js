e6c1b71ba44f77c980af0da85d20d6a4211da7ac
preetiatri <preetiatri.dev@gmail.com>
2026-04-07 22:53:45 +0530

    Updates

--- assets/XMarkIcon-DZVkE_c-.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-aI_ZhAh5.js";const t=r=>o.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",strokeWidth:1.5,stroke:"currentColor",...r,children:o.jsx("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M6 18L18 6M6 6l12 12"})});export{t as X};
